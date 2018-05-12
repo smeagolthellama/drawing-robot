637ff323d181c97f621dde654e23420fb552b7b8
Jozsef Sallai <jozsef.sallai@intomedia.hu>
2018-05-12 22:56:23 +0300

    update README and comments in makeNew.sh

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# drawing-robot
-code for a raspberry pi-based drawing robot, that uses evolvotron to generate the images.
+# Drawing Robot (W.I.P.)
+Source code for a Raspberry Pi-based drawing robot. This robot will draw lines from a black-and-white image generated with Evolvotron and ImageMagick, based on the coordinates of all white pixels.
+
+## Requirements:
+ * Evolvotron
+ * ImageMagick
+ * Go
+
+## Getting Started
+
+Before doing anything make sure you have the correct packages installed on your machine:
+
+```
+sudo apt update && sudo apt upgrade
+sudo apt install evolvotron imagemagick golang-go
+```
+
+Clone the repository:
+
+```
+git clone git@github.com:smeagolthellama/drawing-robot
+cd drawing-robot
+```
+
+To generate a new image and a file with coordinates, run:
+
+```
+./makeNew.sh
+```
--- makeNew.sh
+++ makeNew.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# example for generating images from manpage. sample included.
-#evolvotron_mutate -g | evolvotron_render -s 1024x1024 function.ppm
 # We want a log of our images, and to not use very much space, so I recomend doing something like `date +%s`.png or jpg (if supported)
 name=$(date +%s)
 echo -n "generating image ($name.png)... "
 evolvotron_mutate -g | evolvotron_render -s 1024x1024 "$name.png"
 echo "done."
+
 # use imagemagick to get lines, instead of colours. (no idea what the numbers do, but all examples had them, so leaving them in.)
 echo -n "converting to drawable... "
 convert "$name.png" -canny 0x1+10%+30% "$name.edges.png"
 echo "done."
 
+# use the Go script to generate the coordinates of all white pixels
 echo -n "obtaining coordinates... "
 go run CoordGen.go "$name.edges.png"
