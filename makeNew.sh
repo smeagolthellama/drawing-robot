d842110f4745b939c889b5631c15ef46f46544fd
M. Gardner <magardner2010@gmail.com>
2018-05-12 13:11:11 +0000

    add initial, working image-generation script.

--- makeNew.sh
+++ makeNew.sh
@@ -3,3 +3,4 @@
 # example for generating images from manpage. sample included.
 #evolvotron_mutate -g | evolvotron_render -s 1024x1024 function.ppm
 # We want a log of our images, and to not use very much space, so I recomend doing something like `date +%s`.png or jpg (if supported)
+evolvotron_mutate -g | evolvotron_render -s 1024x1024 `date +%s`.png
